158c00fa353463f16a568a56917af6b7ea6a465a
Tauha Imran <161915837+tauhaimran@users.noreply.github.com>
2025-11-12 22:54:23 +0500

    client server test 2 ( passed ) + diagnostic files in scripts/

--- scripts/gen_ca.py
+++ scripts/gen_ca.py
@@ -88,3 +88,4 @@ if __name__ == "__main__":
 ############################
 # to run from command line:
 # python scripts/gen_ca.py --ca-name MyRootCA --output-dir certs
+############################
