e53c3a5c1ca92c037d8c3cee9d639f687ec291b0
Tauha Imran <161915837+tauhaimran@users.noreply.github.com>
2025-11-14 13:02:32 +0500

    sql , db , .gitignore and testing output setups

---
+++ app/storage/queries.sql
@@ -0,0 +1,30 @@
+# run these ONCE ONLY
+CREATE DATABASE securechat;
+
+CREATE USER 'scuser'@'localhost' IDENTIFIED BY 'scpass';
+GRANT ALL PRIVILEGES ON securechat.* TO 'scuser'@'localhost';
+FLUSH PRIVILEGES;
+###################################
+
+#this shows my databases
+SHOW DATABASES;
+
+#this shows all sql users
+SELECT user, host FROM mysql.user;
+
+#this shows priveliges of users
+SHOW GRANTS FOR 'scuser'@'localhost';
+
+#this shows all the tables made ( after executing db.py )
+USE securechat;
+SHOW TABLES;
+DESCRIBE users;
+
+#view registered users
+SELECT id, username, HEX(salt) AS salt_hex, pwd_hash FROM users;
+
+#view all users
+USE securechat;
+SELECT * FROM users;
+
+
